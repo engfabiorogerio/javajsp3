f40770563ca51d9972dfdead84163892d2931b99
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-08-24 18:12:33 -0300

    Implements CREATE and READ methods

--- projetoJSP3/src/main/java/com/engfabiorogerio/projetojsp3/controller/Controller.java
+++ projetoJSP3/src/main/java/com/engfabiorogerio/projetojsp3/controller/Controller.java
@@ -1,9 +1,14 @@
 package com.engfabiorogerio.projetojsp3.controller;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import com.engfabiorogerio.projetojsp3.model.DAO;
+import com.engfabiorogerio.projetojsp3.model.JavaBeans;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,11 +18,12 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	/**
 	 * Default constructor.
@@ -33,12 +39,34 @@ public class Controller extends HttpServlet {
 			System.out.println(action);
 			if(action.equals("/main")) {
 				contatos(request, response);
+			}else if(action.equals("/insert")) {
+				novoContato(request, response);
+			}else {
+				response.sendRedirect("index.html");
 			}
 	}
 
+	//Listar Contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			response.sendRedirect("agenda.jsp");
+			ArrayList<JavaBeans> lista = dao.listarContatos();
+			
+			request.setAttribute("contatos", lista);
+			RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+			rd.forward(request, response);
+			
+	}
+	
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			
+			
+			contato.setNome(request.getParameter("nome"));
+			contato.setFone(request.getParameter("fone"));
+			contato.setEmail(request.getParameter("email"));
+			dao.inserirContato(contato);
+			
+			response.sendRedirect("main");
 	}
 
 	
--- projetoJSP3/src/main/webapp/agenda.jsp
+++ projetoJSP3/src/main/webapp/agenda.jsp
@@ -1,5 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+   <%@ page import="java.util.ArrayList"%>
+   <%@ page import="com.engfabiorogerio.projetojsp3.model.JavaBeans" %>
+   <%
+   
+   		ArrayList<JavaBeans> lista = (ArrayList<JavaBeans>)request.getAttribute("contatos");
+   
+   		   
+   %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,6 +21,29 @@
 	<h1>Agenda de Contatos</h1>
 	
 	<a href="novo_contato.html" class="botao1">Novo Contato</a>
+	
+	<table class="tabela" style="margin-top: 30px; width: 70vw;">		
+		<thead style="background-color: #66bbff; color: white;">
+			<tr>
+				<th style="border: 1px solid gray;">Id</th>
+				<th style="border: 1px solid gray;">Nome</th>
+				<th style="border: 1px solid gray;">Fone</th>
+				<th style="border: 1px solid gray;">Email</th>
+			</tr>
+		</thead>
+		<tbody>
+			<%for(int i = 0; i < lista.size(); i++){ %>
+				
+				<tr >
+					<td style="border: 1px solid gray; text-align: center;"><%= lista.get(i).getIdcon() %></td>
+					<td style="border: 1px solid gray;"><%= lista.get(i).getNome() %></td>
+					<td style="border: 1px solid gray;"><%= lista.get(i).getFone() %></td>
+					<td style="border: 1px solid gray;"><%= lista.get(i).getEmail() %></td>
+				</tr>
+				
+			<%} %>
+		</tbody>
+	</table>
 
 </body>
 </html>
